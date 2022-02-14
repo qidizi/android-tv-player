13c785ee56d79544e0d96ef95398386fa723be7f
qidizi <qidizi@qq.com>
2022-02-14 22:00:50 +0800

    坚果投影 android 6可以使用

--- app/src/main/java/qidizi/tv/MainActivity.java
+++ app/src/main/java/qidizi/tv/MainActivity.java
@@ -17,13 +17,15 @@ import com.google.zxing.BarcodeFormat;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity
+{
     final private static int tipId = 100000;
     final private static int qrCodeId = tipId + 1;
     private static MainActivity mainActivity = null;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+	{
         super.onCreate(savedInstanceState);
         // 隐藏标题
         this.getActionBar().hide();
@@ -36,18 +38,18 @@ public class MainActivity extends Activity {
         ImageView imageView = new ImageView(this);
         imageView.setId(qrCodeId);
         FrameLayout.LayoutParams layout = new FrameLayout.LayoutParams(
-                FrameLayout.LayoutParams.WRAP_CONTENT,
-                FrameLayout.LayoutParams.WRAP_CONTENT,
-                Gravity.BOTTOM | Gravity.CENTER
+			FrameLayout.LayoutParams.WRAP_CONTENT,
+			FrameLayout.LayoutParams.WRAP_CONTENT,
+			Gravity.BOTTOM | Gravity.CENTER
         );
         this.setContentView(imageView, layout);
         // 创建提示view
         TextView textView = new TextView(this);
         textView.setGravity(Gravity.START);
         layout = new FrameLayout.LayoutParams(
-                FrameLayout.LayoutParams.WRAP_CONTENT,
-                FrameLayout.LayoutParams.WRAP_CONTENT,
-                Gravity.TOP | Gravity.CENTER
+			FrameLayout.LayoutParams.WRAP_CONTENT,
+			FrameLayout.LayoutParams.WRAP_CONTENT,
+			Gravity.TOP | Gravity.CENTER
         );
         addContentView(textView, layout);
         textView.setId(tipId);
@@ -62,11 +64,13 @@ public class MainActivity extends Activity {
         textView.setMovementMethod(new ScrollingMovementMethod());
     }
 
-    private void refreshLayout() {
+    private void refreshLayout()
+	{
         ImageView imageView = findViewById(qrCodeId);
         TextView textView = findViewById(tipId);
 
-        if (null == imageView || null == textView) {
+        if (null == imageView || null == textView)
+		{
             MyApplication.toast("二维码及提示框不存在");
             return;
         }
@@ -77,76 +81,97 @@ public class MainActivity extends Activity {
         int height = metrics.heightPixels;
         FrameLayout.LayoutParams layout;
 
-        if (width < height) {
+        if (width < height)
+		{
             // 如果是高屏/正方形，上下布局；
             int minSize = Math.min(height / 2, width);
             layout = new FrameLayout.LayoutParams(
-                    minSize,
-                    minSize,
-                    Gravity.BOTTOM | Gravity.CENTER
+				minSize,
+				minSize,
+				Gravity.BOTTOM | Gravity.CENTER
             );
             imageView.setLayoutParams(layout);
             layout = new FrameLayout.LayoutParams(
-                    FrameLayout.LayoutParams.MATCH_PARENT,
-                    height / 2,
-                    Gravity.TOP | Gravity.CENTER
+				FrameLayout.LayoutParams.MATCH_PARENT,
+				height / 2,
+				Gravity.TOP | Gravity.CENTER
             );
-        } else {
+        }
+		else
+		{
             // 如果是宽屏，左右
             int minSize = Math.min(width / 2, height);
             layout = new FrameLayout.LayoutParams(
-                    minSize,
-                    minSize,
-                    Gravity.START | Gravity.CENTER
+				minSize,
+				minSize,
+				Gravity.START | Gravity.CENTER
             );
             imageView.setLayoutParams(layout);
             layout = new FrameLayout.LayoutParams(
-                    width / 2,
-                    FrameLayout.LayoutParams.MATCH_PARENT,
-                    Gravity.END | Gravity.CENTER
+				width / 2,
+				FrameLayout.LayoutParams.MATCH_PARENT,
+				Gravity.END | Gravity.CENTER
             );
         }
         textView.setLayoutParams(layout);
     }
 
-    private void refreshQr() {
+    private void refreshQr()
+	{
         String url = MyApplication.getHttpUrl();
-        if (null == url) {
+        if (null == url)
+		{
             return;
         }
         // 创建二维码对象
         QRCodeWriter writer = new QRCodeWriter();
-        try {
+        try
+		{
             int size = 1024;
             BitMatrix bitMatrix = writer.encode(url, BarcodeFormat.QR_CODE, size, size);
             int width = bitMatrix.getWidth();
             int height = bitMatrix.getHeight();
             Bitmap bmp = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565);
-            for (int x = 0; x < width; x++) {
-                for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++)
+			{
+                for (int y = 0; y < height; y++)
+				{
                     bmp.setPixel(x, y, bitMatrix.get(x, y) ? Color.BLACK : Color.WHITE);
                 }
             }
             ((ImageView) findViewById(qrCodeId)).setImageBitmap(bmp);
             tip("请扫码访问 " + url);
-        } catch (Exception e) {
+        }
+		catch (Exception e)
+		{
             e.printStackTrace();
             tip("创建二维码失败:" + e.getMessage());
         }
     }
 
-    private void tip(String msg) {
-        try {
-            ((TextView) findViewById(tipId)).setText(msg);
-        } catch (Exception e) {
-            e.printStackTrace();
-            // 如果在activity中显示失败，就换这个方式
-            MyApplication.toast(msg);
-        }
+    private void tip(final String msg)
+	{
+		mainActivity.runOnUiThread(new Runnable() {
+				@Override
+				public void run()
+				{
+					try
+					{
+						((TextView) findViewById(tipId)).setText(msg);
+					}
+					catch (Exception e)
+					{
+						e.printStackTrace();
+						// 如果在activity中显示失败，就换这个方式
+						MyApplication.toast(msg);
+					}
+				}
+			});
     }
 
     @Override
-    protected void onResume() {
+    protected void onResume()
+	{
         refreshQr();
         // 这个不能放到 super.onResume 后面，否则会导致出错
         // android.os.BinderProxy cannot be cast to android.app.servertransaction.ClientTransaction
@@ -156,31 +181,36 @@ public class MainActivity extends Activity {
     }
 
     @Override
-    public void onWindowAttributesChanged(WindowManager.LayoutParams params) {
+    public void onWindowAttributesChanged(WindowManager.LayoutParams params)
+	{
         // 放在这，目前小米 9 切成悬浮布局异常
         refreshLayout();
         super.onWindowAttributesChanged(params);
     }
 
     @Override
-    protected void onDestroy() {
+    protected void onDestroy()
+	{
         mainActivity = null;
         super.onDestroy();
     }
 
     @Override
-    protected void onStart() {
+    protected void onStart()
+	{
         super.onStart();
         MyApplication.setInBackground(false);
     }
 
     @Override
-    protected void onPause() {
+    protected void onPause()
+	{
         super.onPause();
         MyApplication.setInBackground(true);
     }
 
-    public static MainActivity getMe() {
+    public static MainActivity getMe()
+	{
         return mainActivity;
     }
 }
--- app/src/main/java/qidizi/tv/MyApplication.java
+++ app/src/main/java/qidizi/tv/MyApplication.java
@@ -18,7 +18,8 @@ import java.net.*;
 import java.util.Enumeration;
 import java.util.regex.Pattern;
 
-public class MyApplication extends Application {
+public class MyApplication extends Application
+{
     // ~/android/platform-tools/adb forward tcp:8080 tcp:8080 来影射avd中的端口到物理机
     // 一般的系统是不允许普通应用开启1024以下的端口的
     private final static int PORT = 8080;
@@ -28,46 +29,66 @@ public class MyApplication extends Application {
     private boolean inBackground = false;
 
     @Override
-    public void onCreate() {
+    public void onCreate()
+	{
         super.onCreate();
         application = this;
         createHttp();
     }
 
-    private void createHttp() {
-        if (null != http) {
+    private void createHttp()
+	{
+        if (null != http)
+		{
             toast("http服务已存在");
             return;
         }
 
-        new Thread(() -> {
-            try {
-                // 使用端口转发功能,把虚拟机avd端口转发到开发机的8080上,就可以使用 http://127.0.0.1:8080 来访问
-                // ~/android/platform-tools/adb forward tcp:8080 tcp:8080
-                // 同时只允许一个请求,其它会被拒绝
-                int maxConnections = 1;
-                http = new ServerSocket(PORT, maxConnections);
-                http.setReuseAddress(true);
-            } catch (Exception e) {
-                httpClose();
-                toast("http服务启动失败：" + e.getMessage());
-            }
+		// aide 不支持 Lambda
+        new Thread(new Runnable() {
+				@Override
+				public void run()
+				{
+					try
+					{
+						// 使用端口转发功能,把虚拟机avd端口转发到开发机的8080上,就可以使用 http://127.0.0.1:8080 来访问
+						// ~/android/platform-tools/adb forward tcp:8080 tcp:8080
+						// 同时只允许一个请求,其它会被拒绝
+						int maxConnections = 1;
+						http = new ServerSocket(PORT, maxConnections);
+						http.setReuseAddress(true);
+					}
+					catch (Exception e)
+					{
+						httpClose();
+						toast("http服务启动失败：" + e.getMessage());
+					}
 
-            if (!http.isBound()) {
-                toast(PORT + " 端口绑定失败");
-                httpClose();
-                return;
-            }
+					if (!http.isBound())
+					{
+						toast(PORT + " 端口绑定失败");
+						httpClose();
+						return;
+					}
 
-            //noinspection
-            while (!http.isClosed())
-                accept(http);
-        }).start();
+					//noinspection
+					while (!http.isClosed())
+						accept(http);
+				}}).start();
+
+
     }
 
-    private void accept(ServerSocket http) {
+    private void accept(ServerSocket http)
+	{
         if (!application.hasNet()) return;
-        try (Socket client = http.accept(); InputStream is = client.getInputStream()) {
+		//aide 不支持 try管理res
+		Socket client=null;
+		InputStream is =null;
+        try
+		{
+			client = http.accept();
+			is = client.getInputStream();
             client.setSoTimeout(1000 * 3);
             int maxGet = 1024 * 2;
             // get 支持的长度，一般的 requestString 并不需要太长
@@ -86,9 +107,12 @@ public class MyApplication extends Application {
             Uri uri = Uri.parse(str);
             String videoUrl = uri.getQueryParameter("url");
 
-            if (null != videoUrl && !videoUrl.isEmpty()) {
+            if (null != videoUrl && !videoUrl.isEmpty())
+			{
                 application.playUrl(videoUrl);
-            } else if (str.contains("url=")) {
+            }
+			else if (str.contains("url="))
+			{
                 // 只有提交时才提醒
                 toast("get请求缺失：&url=视频url");
             }
@@ -102,109 +126,166 @@ public class MyApplication extends Application {
                 byte[] body = new byte[bodyLen];
                 bodyLen = htmlIs.read(body);
                 client.getOutputStream().write(
-                        (
-                                "HTTP/1.1 200 OK\r\n"
-                                        + "Content-Type: text/html;charset=utf-8\r\n"
-                                        + "Content-Length: " + bodyLen + "\r\n"
-                                        + "\r\n"
-                        ).getBytes()
+					(
+					"HTTP/1.1 200 OK\r\n"
+					+ "Content-Type: text/html;charset=utf-8\r\n"
+					+ "Content-Length: " + bodyLen + "\r\n"
+					+ "\r\n"
+					).getBytes()
                 );
 
                 if (bodyLen > 0)
                     client.getOutputStream().write(body);
             }
-        } catch (Exception e) {
+        }
+		catch (Exception e)
+		{
             e.printStackTrace();
             // 如果联网权限未得到，这里会报
             // android.system.ErrnoException: accept failed: EACCES (Permission denied)
             toast("处理请求失败:" + e.getMessage());
         }
+		finally
+		{
+			try
+			{
+				if (is != null)is.close();
+			}
+			catch (Exception ignore)
+			{}
+			try
+			{
+				if (client != null)client.close();
+			}
+			catch (Exception ignore)
+			{}
+		}
     }
 
-    static public String getHttpUrl() {
+    static public String getHttpUrl()
+	{
         String ip = null;
         Enumeration<NetworkInterface> en;
 
-        try {
+        try
+		{
             en = NetworkInterface.getNetworkInterfaces();
-        } catch (Exception e) {
+        }
+		catch (Exception e)
+		{
             toast("获取电视ip失败：" + e.getMessage());
             return null;
         }
 
-        while (en.hasMoreElements()) {
+        while (en.hasMoreElements())
+		{
             NetworkInterface nif = en.nextElement();
-            for (Enumeration<InetAddress> enumIps = nif.getInetAddresses(); enumIps.hasMoreElements(); ) {
+            for (Enumeration<InetAddress> enumIps = nif.getInetAddresses(); enumIps.hasMoreElements();)
+			{
                 InetAddress inetAddress = enumIps.nextElement();
                 if (inetAddress.isLoopbackAddress() || !(inetAddress instanceof Inet4Address)) continue;
                 String address = inetAddress.getHostAddress();
-                if (address.startsWith("10.") || address.startsWith("192.168.")) {
+                if (address.startsWith("10.") || address.startsWith("192.168."))
+				{
                     ip = address;
                     break;
                 }
 
-                if (address.startsWith("172.")) {
+                if (address.startsWith("172."))
+				{
                     int sub = Integer.parseInt(address.split("\\.")[1]);
 
-                    if (sub >= 16 && sub <= 31) {
+                    if (sub >= 16 && sub <= 31)
+					{
                         ip = address;
                         break;
                     }
                 }
             }
 
-            if (ip != null) {
+            if (ip != null)
+			{
                 break;
             }
         }
 
-        if (null == ip) {
+        if (null == ip)
+		{
             toast("推算电视的ip失败");
             return null;
         }
 
         return String.format(
-                // 防止idea报换https
-                "http" + "://%s:%s/?noCache=%s",
-                ip,
-                PORT,
-                System.currentTimeMillis()
+			// 防止idea报换https
+			"http" + "://%s:%s/?noCache=%s",
+			ip,
+			PORT,
+			System.currentTimeMillis()
         );
     }
 
-    private void playUrl(final String url) {
-        // 应用在后台时，无法操作剪切板
-        // https://stackoverflow.com/questions/65632024/is-copy-to-clipboard-when-app-in-background-restricted-in-android
-        ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
-        ClipData clip = ClipData.newPlainText("video", url);
-        clipboard.setPrimaryClip(clip);
+    private void playUrl(final String url)
+	{
+		final MainActivity mainActivity = MainActivity.getMe();
 
-        Uri uri = Uri.parse(url);
-        final Intent intent = new Intent(Intent.ACTION_VIEW);
-        intent.setDataAndType(uri, "video/*");
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        if (null == mainActivity)
+		{
+			toast("activity为null");
+			return;
+		}
 
-        if (intent.resolveActivity(getPackageManager()) != null) {
-            startActivity(intent);
-        } else {
-            toast("无法调起外围视频播放器");
-            return;
-        }
+        mainActivity.runOnUiThread(new Runnable() {
+				@Override
+				public void run()
+				{
 
-        toast((inBackground ? "" : "已复制至剪切板！") + "正送至外置播放器：" + url);
+					// 应用在后台时，无法操作剪切板
+					// https://stackoverflow.com/questions/65632024/is-copy-to-clipboard-when-app-in-background-restricted-in-android
+					ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+					ClipData clip = ClipData.newPlainText("video", url);
+					clipboard.setPrimaryClip(clip);
 
+					Uri uri = Uri.parse(url);
+					final Intent intent = new Intent(Intent.ACTION_VIEW);
+					intent.setDataAndType(uri, "video/*");
+					intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+					if (intent.resolveActivity(getPackageManager()) != null)
+					{
+						try
+						{				
+						    // 坚果投影 6.x系统要放到ui进程
+							startActivity(intent);
+						}
+						catch (Exception e)
+						{
+							toast("startActivity异常：" + e.getMessage());
+						}
+					}
+					else
+					{
+						toast("无法调起外围视频播放器");
+						return;
+					}
+
+					toast((inBackground ? "" : "已复制至剪切板！") + "正送至外置播放器：" + url);
+
+				}});
     }
 
-    private boolean hasNet() {
+    private boolean hasNet()
+	{
         // 在小米 9，未授权访问网络，也返回了成功，但是首次会弹出授予网络的提示
-        if (PackageManager.PERMISSION_DENIED == checkCallingOrSelfPermission(Manifest.permission.INTERNET)) {
+        if (PackageManager.PERMISSION_DENIED == checkCallingOrSelfPermission(Manifest.permission.INTERNET))
+		{
             toast("请授予本应用网络权限再试");
             return false;
         }
 
         ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
 
-        if (cm.getActiveNetworkInfo() == null || !cm.getActiveNetworkInfo().isConnected()) {
+        if (cm.getActiveNetworkInfo() == null || !cm.getActiveNetworkInfo().isConnected())
+		{
             toast("请授予本应用网络权限再试");
             return false;
         }
@@ -212,28 +293,40 @@ public class MyApplication extends Application {
         return true;
     }
 
-    static public void toast(final String str) {
+    static public void toast(final String str)
+	{
         final MainActivity mainActivity = MainActivity.getMe();
 
         if (null == mainActivity) return;
-        mainActivity.runOnUiThread(() -> {
-            try {
-                // 确保操作处于ui线程
-                Toast.makeText(mainActivity, str, Toast.LENGTH_SHORT).show();
-            } catch (Exception e) {
-                System.out.printf("toast 内容：%s，异常如下", str);
-                e.printStackTrace();
-            }
-        });
+        mainActivity.runOnUiThread(new Runnable() {
+				@Override
+				public void run()
+				{
+					try
+					{
+						// 确保操作处于ui线程
+						Toast.makeText(mainActivity, str, Toast.LENGTH_SHORT).show();
+					}
+					catch (Exception e)
+					{
+						System.out.printf("toast 内容：%s，异常如下", str);
+						e.printStackTrace();
+					}
+				}});
     }
 
-    static private void httpClose() {
-        if (null != http && !http.isClosed()) {
+    static private void httpClose()
+	{
+        if (null != http && !http.isClosed())
+		{
             long start = System.currentTimeMillis();
-            try {
+            try
+			{
                 http.close();
                 http = null;
-            } catch (Exception ignore) {
+            }
+			catch (Exception ignore)
+			{
 
             }
 
@@ -241,7 +334,8 @@ public class MyApplication extends Application {
         }
     }
 
-    static public void setInBackground(boolean b) {
+    static public void setInBackground(boolean b)
+	{
         application.inBackground = b;
     }
 }
